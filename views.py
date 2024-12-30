2e3cf109d52cc9ac6e6d73fb4570754ecb54feb7
Gabriel M. Dutra <gabrieldmdutra@gmail.com>
2024-12-30 04:25:42 -0300

    Add index and '/new' routes

--- views.py
+++ views.py
@@ -1,10 +1,16 @@
 from app import app
 from flask import render_template
+from models.models import Books
 
 @app.route("/")
 def index():
-    return "index"
+    books = Books.query.order_by(Books.id)
+    return render_template('list.html', books=books)
 
 @app.route("/login")
 def login():
     return render_template('login.html')
+
+@app.route("/new")
+def new():
+    return render_template('new.html')
