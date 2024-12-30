8f848a09e8b41cbd4dee174d9be55556f58e93c4
Gabriel M. Dutra <gabrieldmdutra@gmail.com>
2024-12-30 04:26:38 -0300

    Add model to books

--- models/models.py
+++ models/models.py
@@ -6,3 +6,13 @@ class Users(db.Model):
 
     def __repr__(self):
         return f'<Name {self.name}>'
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(40), primary_key=True)
+    author = db.Column(db.String(40), nullable=False)
+    is_read = db.Column(db.Boolean, nullable=False)
+    comments = db.Column(db.String(255), nullable=True)
+
+    def __repr__(self):
+        return f'<Title {self.title}>'
