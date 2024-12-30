7a002582513a09572ed109833b56ca53c410a41a
Gabriel M. Dutra <gabrieldmdutra@gmail.com>
2024-12-30 04:26:18 -0300

    Add route to create a book and change render_template to redirect

--- controllers.py
+++ controllers.py
@@ -1,7 +1,13 @@
 from app import app
+from app import db
+
+from flask import redirect
+from flask import url_for
 from flask import request
-from flask import render_template
+from flask import flash
+
 from models.models import Users
+from models.models import Books
 
 
 @app.route("/auth", methods=['POST'])
@@ -11,6 +17,33 @@ def auth():
     user = Users.query.filter_by(username=username).first()
 
     if user and password == user.password:
-        return render_template('list.html')
+        return redirect(url_for('index'))
 
-    return render_template('login.html')
+    return redirect('login')
+
+@app.route("/create", methods=['POST'])
+def create():
+    title = request.form['title']
+    author = request.form['author']
+    read = request.form['read']
+
+    is_read = False
+
+    if read == "on":
+        is_read = True
+
+    book = Books.query.filter_by(title=title).first()
+    if book:
+        flash('The has already been registered!')
+        return redirect('index')
+
+    new_book = Books(
+        title=title,
+        author=author,
+        is_read=is_read
+    )
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return redirect(url_for('index'))
