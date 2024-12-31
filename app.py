59df4634c3e83f831a1b03324d4d418921cc9dac
Gabriel M. Dutra <gabrieldmdutra@gmail.com>
2024-12-31 16:33:36 -0300

    remove repository module

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask
-from repository.repository import DB
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-db = DB(app)
+db = SQLAlchemy(app)
 
 from views import *
 from controllers import *
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-
--- repository/repository.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-def DB(app):
-    return SQLAlchemy(app)
- 
